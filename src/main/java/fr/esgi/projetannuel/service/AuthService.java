8a5c01abf5092b4848a70701f77ddb12228064e8
PERNICENI Ugo <ugo.perniceni@hotmail.fr>
2021-04-25 22:28:54 +0200

    add auth & resource not found exception

---
+++ src/main/java/fr/esgi/projetannuel/service/AuthService.java
@@ -0,0 +1,66 @@
+package fr.esgi.projetannuel.service;
+
+import fr.esgi.projetannuel.exception.ResourceNotFoundException;
+import fr.esgi.projetannuel.model.Login;
+import fr.esgi.projetannuel.model.Session;
+import fr.esgi.projetannuel.model.User;
+import fr.esgi.projetannuel.repository.SessionRepository;
+import fr.esgi.projetannuel.repository.UserRepository;
+import fr.esgi.projetannuel.security.TokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@Service
+public class AuthService {
+    private final TokenProvider tokenProvider;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+
+    private final UserRepository userRepository;
+    private final SessionRepository sessionRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public AuthService(TokenProvider tokenProvider, AuthenticationManagerBuilder authenticationManagerBuilder, UserRepository userRepository, SessionRepository sessionRepository, PasswordEncoder passwordEncoder) {
+        this.tokenProvider = tokenProvider;
+        this.authenticationManagerBuilder = authenticationManagerBuilder;
+        this.userRepository = userRepository;
+        this.sessionRepository = sessionRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public HttpHeaders createSession(Login login){
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                login.getEmail(),
+                login.getPassword());
+
+        User user = findUserByEmail(login.getEmail());
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        String token = tokenProvider.createToken(authentication);
+
+        sessionRepository.save(new Session(user.getId(), null, token, user));
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+        return httpHeaders;
+    }
+
+    public URI registerUser(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        String id = userRepository.save(user).getId();
+        return ServletUriComponentsBuilder.fromPath("/api/users").path("/{id}").buildAndExpand(id).toUri();
+    }
+
+    public User findUserByEmail(String email){
+        return userRepository.findByEmail(email).orElseThrow(() -> new ResourceNotFoundException("user", email));
+    }
+}
---
+++ src/main/java/fr/esgi/projetannuel/service/UserSecurityService.java
@@ -0,0 +1,37 @@
+package fr.esgi.projetannuel.service;
+
+import fr.esgi.projetannuel.model.User;
+import fr.esgi.projetannuel.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserSecurityService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserSecurityService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User appUser = userRepository.findByEmail(email)
+                .orElseThrow(() -> new AuthenticationServiceException("email " + email + " not found"));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(email)
+                .password(appUser.getPassword())
+                .roles(appUser.getRole().toString())
+                .accountExpired(false)
+                .accountLocked(false)
+                .credentialsExpired(false)
+                .disabled(false)
+                .build();
+    }
+}
