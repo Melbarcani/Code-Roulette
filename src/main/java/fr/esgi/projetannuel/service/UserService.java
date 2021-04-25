279f75a1689f637badd9f9e94fa87907a379ff42
PERNICENI Ugo <ugo.perniceni@hotmail.fr>
2021-04-25 22:28:54 +0200

    add auth & resource not found exception

--- src/main/java/fr/esgi/projetannuel/service/UserService.java
+++ src/main/java/fr/esgi/projetannuel/service/UserService.java
@@ -10,8 +10,10 @@ import javax.transaction.Transactional;
 import java.util.List;
 import fr.esgi.projetannuel.model.User;
 import fr.esgi.projetannuel.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
