e4ab17143a61b632d0080ebd6656835b7ebaa236
PERNICENI Ugo <ugo.perniceni@hotmail.fr>
2021-06-10 17:04:03 +0200

    add enum Language in Exercise :rocket:

--- src/main/java/fr/esgi/projetannuel/model/Exercise.java
+++ src/main/java/fr/esgi/projetannuel/model/Exercise.java
@@ -1,11 +1,9 @@
 package fr.esgi.projetannuel.model;
 
+import fr.esgi.projetannuel.enumeration.Language;
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
@@ -25,6 +23,10 @@ public class Exercise {
     @Column(columnDefinition="text", nullable = false)
     private String code;
 
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Language language;
+
     @Column(nullable = true)
     private LocalDateTime createdAt;
 
@@ -35,9 +37,10 @@ public class Exercise {
         this.createdAt = LocalDateTime.now();
     }
 
-    public Exercise(String title, String code) {
+    public Exercise(String title, String code, Language language) {
         this.title = title;
         this.code = code;
+        this.language = language;
 
         this.createdAt = LocalDateTime.now();
     }
@@ -64,6 +67,14 @@ public class Exercise {
 
     public void setCode(String code) {
         this.code = code;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(Language language) {
+        this.language = language;
     }
 
     public LocalDateTime getCreatedAt() {
