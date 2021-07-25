47df9cc17eb6241485ec6b6a53455e28113fe919
Mohamed EL BARCANI <melbarcani@github.com>
2021-07-25 02:11:22 +0200

    redirect to server

--- src/main/java/fr/esgi/projetannuel/model/Constants.java
+++ src/main/java/fr/esgi/projetannuel/model/Constants.java
@@ -5,6 +5,6 @@ public class Constants {
     }
 
     public static final String EXERCISE = "Exercise";
-    public static final String COMPILER_BASE_URL = "http://localhost:8099/api/compiler";//"http://52.209.183.45:8080/api/compiler";
+    public static final String COMPILER_BASE_URL = "http://52.209.183.45:8080/api/compiler";//"http://localhost:8099/api/compiler";
     public static final String USER = "user";
 }
